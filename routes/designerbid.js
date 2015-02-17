5d9cf5446d2dd31b4f8bfd943043b0d2e2ff1c82
Juli <joosz.julia@gmail.com>
2015-02-17 16:16:44 +0100

    Designerbid corr

--- routes/designerbid.js
+++ routes/designerbid.js
@@ -3,17 +3,16 @@ var getMinValue = require('./admin.js').getMinValue;
 var endAuction = require('./admin.js').endAuction;
 
 var DesignerBid = function(app, io, DesignerBID) {
-    app.post('/bidfordesigner', function(req, res) {
         io.on('connection', function(socket) {
-            socket.on('BIDcheck', function(msg) {
+            socket.on('BIDcheck', function(data) {
                 var value = getCurrentValue();
                 var minValue = getMinValue();
                 if (value) {
                     if (value <= getMinValue) {
                         socket.emit('BIDfail', 'A BID nem kerult rogzitesre');
                     } else {
-                        //endAuction();
-                        console.log('BIDsuccess');
+                        endAuction();
+                        console.log('BIDsuccess: '+ value);
                         socket.emit('BIDsuccess', 'A BID rogzitesre kerult');
                     }
                 } else {
@@ -22,29 +21,6 @@ var DesignerBid = function(app, io, DesignerBID) {
             });
             socket.on('disconnect', function() {});
         });
-
-        var value = getCurrentValue();
-        var minValue = getMinValue();
-        if (value) {
-            if (value <= getMinValue) {
-                //nincs rogzitve a bid
-            } else {
-                endAuction();
-                console.log('endAuction');
-                var newdesignerbid = {
-                    name: req.body.optradio,
-                    osszeg: req.body.designerosszeg,
-                    felado: req.user.TeamID
-                };
-                var designerbid = new DesignerBID(newdesignerbid);
-                designerbid.save();
-            }
-        } else {
-            //nincs rogzitve a bid
-        }
-        console.log('redirect to /personal');
-        res.redirect('/personal');
-    });
 };
 
 exports.DesignerBid = DesignerBid;

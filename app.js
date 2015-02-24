dbd0f741024d059e4a3fa2a29dc7e97cf7159bb3
Khadgar <daniel.szaniszlo@gmail.com>
2015-02-24 22:17:39 +0100

    Reformatted views and results table design

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ var busboy = require('connect-busboy');
 app.set('port', process.env.PORT || 3000);
 app.use(express.cookieParser('dandroid'));
 app.use(express.session({
-		secret : 'cookie_secret'
-	}));
+  secret: 'cookie_secret'
+}));
 app.use(bodyParser.urlencoded({
-		extended : true
-	}));
+  extended: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(busboy());
@@ -59,10 +59,10 @@ require(path.join(__dirname, './routes/designerbid.js')).DesignerBid(app, io, De
 require(path.join(__dirname, './routes/sensorbid.js')).SensorBid(app, io, Teams, SensorBID);
 require(path.join(__dirname, './routes/profile.js')).Profile(app, io, Teams, PriorityList, DesignerBID, Designers, SensorBID);
 require(path.join(__dirname, './routes/admin.js')).Admin(app, Teams, io, Designers, Sensors, PriorityList, DesignerBID);
-require(path.join(__dirname, './routes/upload.js')).Upload(app,Teams, ftpupload, busboy);
+require(path.join(__dirname, './routes/upload.js')).Upload(app, Teams, ftpupload, busboy);
 
 
 //create server
-http.listen(app.get('port'), function () {
-	console.log('BeeSmarter server listening on localhost:' + app.get('port'));
+http.listen(app.get('port'), function() {
+  console.log('BeeSmarter server listening on localhost:' + app.get('port'));
 });
